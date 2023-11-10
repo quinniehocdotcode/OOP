2f3479fec554d929167417b8cff598a562c1723e
quinniehocdotcode <121246495+quinniehocdotcode@users.noreply.github.com>
2023-11-10 11:54:40 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
         hd.Them_hoa_don();
         hd.Xuat_Thong_Tin_Hoa_Don_Vao_File();
     ListProduct product =new ListProduct();
+        // QUí cute test git
     // Scanner sc = new Scanner(System.in);
     // ChiTietHoaDon cthd = new ChiTietHoaDon();
     // cthd.Nhap_chi_tiet_hoa_don();
